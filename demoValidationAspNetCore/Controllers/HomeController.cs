785f1b44873375ef08bf3be708c8c29ccd848765
mihadadene <mi.ihadadene@gmail.com>
2020-04-02 01:05:59 -0400

    first commit

---
+++ demoValidationAspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using demoValidationAspNetCore.Models;
+
+namespace demoValidationAspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Create(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                //Movie m = new Movie();
+                return View("confirm");
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
